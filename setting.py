82b26f9478491addb55c9afec6c6eae826c4f5a3
Maksim <88387592+flotam61@users.noreply.github.com>
2022-02-11 14:06:37 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -7,8 +7,8 @@ db = 'postgresql://flores:zxc@localhost:5432/bot'
 engine = sqlalchemy.create_engine(db)
 connection = engine.connect()
 
-token = 'e439ee2f8ac14b6eed53eb7034a4ea911d6980016b1e9daa11bf256700d8f309dd1b2da5f07aa48e7e582'
-token_user = "9a3e3d12366ac8a3ccfaf2ea00a060b14d2a6235ddb4e1134c22e0156b555f0d4cfb16fca109cc4b8700f"
+token = ''
+token_user = ""
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
